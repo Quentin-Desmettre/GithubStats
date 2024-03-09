c681e3d499cb0daccc8ab181cebb9eeb6dcde81f
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2024-03-09 15:46:31 +0100

    feat: added support for https

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
+import * as fs from 'fs';
+
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { INestApplication } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from './types/environmentVariables';
 
+config();
+
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  let app: INestApplication;
+
+  if (process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH) {
+    const httpsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+    app = await NestFactory.create(AppModule, { httpsOptions });
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
 
   const port = app.get(ConfigService<EnvironmentVariables>).get<number>('API_PORT') || 3000;
+  await app.listen(port);
+
   // eslint-disable-next-line no-console
   console.log(`Server is running on port ${port}`);
-  await app.listen(port);
 }
 bootstrap();
